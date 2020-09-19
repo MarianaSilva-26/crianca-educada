180ee41700a7e6b78fd76a2f9e8a5adf9e0bd206
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-19 17:43:25 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace crianca_educada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string educado;
+
+            Console.WriteLine("Me diga o que você pensa sobre o aluno que não faz a lição de casa:");
+            educado = Console.ReadLine();
+            Console.Write("\n ");
+            //<---fim(digite)--->//
+
+            //<---inicio(substituição)--->//
+            Console.WriteLine("eu sou uma crinça educada. no seu lugar eu diria:"); 
+            educado = Console.ReadLine();
+            Console.WriteLine($"{educado}"
+            .Replace("chato" , "#@$%*!&")
+
+            .Replace("chto" , "#@$%*!&")
+
+            .Replace("feio" , "#@$%*!&")
+
+            .Replace("tonto" , "#@$%*!&")
+
+            .Replace("paspalho" , "#@$%*!&")
+
+            .Replace("pentelho" , "#@$%*!&")
+
+            .Replace("burro" , "#@$%*!&")
+
+            .Replace("boboca" , "#@$%*!&")
+
+            .Replace("bocó" , "#@$%*!&")
+
+            .Replace("palerma" , "#@$%*!&")
+
+            .Replace("panaca" , "#@$%*!&")
+
+            .Replace("besta" , "#@$%*!&"));
+            //<---fim(subtituição)--->//
+
+            //<---inicio(sair)--->//
+            Console.WriteLine("pressione enter para finalizar");
+            Console.ReadKey();
+            //<---fim(sair)--->//
+        }
+    }
+}
